c4437ac24b8f50b26bbab224756eb44dd8e147fd
Adheena Chacko <amchacko@udel.edu>
2019-09-27 21:22:49 -0400

    adds search bar in dashboard

--- mobile/src/Screens/Dashboard.js
+++ mobile/src/Screens/Dashboard.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
 import { Button } from 'reactstrap';
+import ReactSearchBox from 'react-search-box';
+
 
  
 class Dashboard extends Component{
     state ={}
+
+    data = [
+        {
+          key: 'john',
+          value: 'John Doe',
+        },
+        {
+          key: 'jane',
+          value: 'Jane Doe',
+        },
+        {
+          key: 'mary',
+          value: 'Mary Phillips',
+        },
+        {
+          key: 'robert',
+          value: 'Robert',
+        },
+        {
+          key: 'karius',
+          value: 'Karius',
+        },
+      ]
 
 
     nextPage = ()  => {
@@ -13,8 +38,14 @@ class Dashboard extends Component{
     render() {
         return(
             <>
-                <h2>This is the Dashboard</h2>   
-                <Button color="danger" onClick={this.nextPage}>Profile</Button>
+            <h2>This is the Dashboard</h2>   
+            <Button color="danger" onClick={this.nextPage}>Profile</Button>
+            <ReactSearchBox
+                placeholder="Placeholder"
+                value="Doe"
+                data={this.data}
+                callback={record => console.log(record)}
+            />
             </>
         );
     };
