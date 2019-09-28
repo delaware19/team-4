51b213b21142526d47502aee4e268318107fa38d
kurnal <kurnalsaini@Kurnals-MacBook-Pro-612.local>
2019-09-27 20:44:55 -0400

    package that has mtaerial ui for react

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/screens/Profile.js"
           target="_blank"
           rel="noopener noreferrer"
         >
