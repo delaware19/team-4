6fe3fab9bf1c2a629324dcd773ae6d96e127719f
Adheena Chacko <amchacko@udel.edu>
2019-09-27 23:39:50 -0400

    adds search bar with search functionalities

--- mobile/src/Screens/Dashboard.js
+++ mobile/src/Screens/Dashboard.js
@@ -1,41 +1,95 @@
 import React, {Component} from 'react';
-import { Button, Container, Row, Col } from 'reactstrap';
-import ReactSearchBox from 'react-search-box';
+import { Button, Container, Row, Col, Input, Card, CardText, CardBody} from 'reactstrap';
+// import ReactSearchBox from 'react-search-box';
+import axios from 'axios'
 
-
- 
 class Dashboard extends Component{
-    state ={}
 
-    data = [
-        {
-          key: 'john',
-          value: 'John Doe',
-        },
-        {
-          key: 'jane',
-          value: 'Jane Doe',
-        },
-        {
-          key: 'mary',
-          value: 'Mary Phillips',
-        },
-        {
-          key: 'robert',
-          value: 'Robert',
-        },
-        {
-          key: 'karius',
-          value: 'Karius',
-        },
-      ]
+    state ={
+        dummyUser:[],
+        dummyTitle:[],
+        text: ' ',
+        result: []
+    }
+
+    handleChange = (event) => {
+        console.log(this.state.text)
+
+        const text = event.target.value.toLowerCase()
+        const newResult = []
+        //dummy user here is the "keyword"
+        //text is the input of the searchbox
+
+        for(let index = 0; index < this.state.dummyUser.length; index++){
+            if(this.state.dummyUser[index].toLowerCase().includes(text)){
+                newResult.push(this.state.dummyUser[index])
+            }
+        }
+        
+        //put results of the search into the result[] array
+        this.setState({result:newResult})
+    }
+
+    componentDidMount(){
+        this._isMounted = true;
+
+        axios.get( 'https://jsonplaceholder.typicode.com/albums', {} )
+            .then( response => {
+                if(this._isMounted){
+                    this.setState({ dummyUser: response.data[0].Id, dummyTitle: response.data[0].title})
+                    console.log(this.state.dummyUser)
+                }
+            } )
+            .catch(error => {
+                console.log(error);
+            });
+        }
+
+    // data = [
+    //     {
+    //       key: 'john',
+    //       value: 'John Doe',
+    //     },
+    //     {
+    //       key: 'jane',
+    //       value: 'Jane Doe',
+    //     },
+    //     {
+    //       key: 'mary',
+    //       value: 'Mary Phillips',
+    //     },
+    //     {
+    //       key: 'robert',
+    //       value: 'Robert',
+    //     },
+    //     {
+    //       key: 'karius',
+    //       value: 'Karius',
+    //     },
+    //   ]
 
 
     nextPage = ()  => {
         this.props.history.push('/profile')
       }
 
+
+      
+    //   renderCategories(){
+    //     return this.state.result.map(story => {
+    //         return (
+    //             <Card key={category.categories.name}>
+    //                 <CardBody>
+    //                     <CardText>{category.categories.name}</CardText>
+    //                 </CardBody>
+    //             </Card>
+    //         )
+    //     })
+    // }
+
+
     render() {
+        // let resp = this.state.isLoading ? <Spinner color="secondary" /> : this.renderCategories()
         return(
             <>
             <Container>
@@ -47,12 +101,18 @@ class Dashboard extends Component{
                         <Button color="danger" onClick={this.nextPage}>Profile</Button>
                     </Col>
                     <Col md="8">
-                        <ReactSearchBox
+                        
+                        {/* {resp} */}
+                        <Input placeholder="search stories" onChange={this.handleChange}/>
+
+                        {/* <Dropdown/> */}
+
+                        {/* <ReactSearchBox
                             placeholder="Search for Stories"
                             value="Doe"
                             data={this.data}
                             callback={record => console.log(record)}
-                        />
+                        /> */}
                     </Col>
                 </Row>
             </Container> 
