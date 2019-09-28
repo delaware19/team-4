ead3a7459717fdff34e957d90a14ecc853b55922
Hang Nguyen <Hang.panbee@gmail.com>
2019-09-28 04:29:04 -0400

    hello

--- canvas/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import ImagesReducer from './ImagesReducer';
-import canvasReducer from './CanvasReducer';
-
-export default combineReducers({
-  imagesState: ImagesReducer,
-  canvasState: canvasReducer,
-});
