bd244915e10e96c59e905cc1f4303b367aa32daa
kurnal <kurnalsaini@Kurnals-MacBook-Pro-612.local>
2019-09-27 21:59:40 -0400

    forms can now bind themselves to the state and be sent as a whole JSON file

--- mobile/src/Screens/Profile.js
+++ mobile/src/Screens/Profile.js
@@ -2,32 +2,60 @@ import React, { Component } from 'react';
 
 class Profile extends Component {
     constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state =
+            {
+                childName: '',
+                careTakers: [],
+                pronoun: '',
+                age: 0,
+            };
+
+        this.handleChildName = this.handleChildName.bind(this);
+        this.handleAge = this.handleAge.bind(this);
+        this.handlePronouns = this.handlePronouns.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
+
+    handleChildName(event) {
+        this.setState({ childName: event.target.value });
     }
-  
+
+    handleAge(event) {
+        this.setState({ age: event.target.value });
+    }
+
+    handlePronouns(event) {
+        this.setState({ pronoun: event.target.value });
+    }
+
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+        alert('A name was submitted: ' + this.state.childName);
+        event.preventDefault();
     }
-  
+
     render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
+        return (
+            <form onSubmit={this.handleSubmit}>
+
+
+                <input type="text" value={this.state.childName} onChange={this.handleChildName} />
+
+                <input type="text" value={this.state.age} onChange={this.handleAge} />
+
+                <select value={this.state.pronoun} onChange={this.handlePronouns}>
+                    <option value="he/his">he/his</option>
+                    <option value="she/her">she/her</option>
+                    <option value="they/them">they/them</option>
+                    <option value="third_person">third person</option>
+                </select>
+
+
+
+
+                <input type="submit" value="Submit" />
+            </form>
+        );
     }
-  }
-  export default Profile;
+}
+export default Profile;
